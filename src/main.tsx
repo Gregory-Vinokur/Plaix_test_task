9bb568fd3fae2300632e387e5b85e783bc0c1b97
Gregory-Vinokur <grisha-vinokur@mail.ru>
2023-07-17 16:59:02 +0300

    feat: add frontend for FeedBackForm

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FeedBackForm from './FeedBackForm';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <FeedBackForm />
+  </React.StrictMode>
+);
